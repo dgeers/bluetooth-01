247a7f5b084b636f41a3eb9cf2b030c120e0b307
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 12:06:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(200)
-    basic.showIcon(IconNames.Chessboard)
-    basic.pause(200)
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(200)
+    while (true) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(200)
+        basic.showIcon(IconNames.Chessboard)
+        basic.pause(200)
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.pause(200)
+    }
 })
-basic.forever(function () {
-	
-})
+basic.showIcon(IconNames.No)
