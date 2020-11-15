81de3fa39796e1eeebb5467cf8d4c01b6943f009
Douglas Geers <dgeers@brooklyn.cuny.edu>
2020-11-15 12:04:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.Chessboard)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(200)
+})
+basic.forever(function () {
+	
+})
